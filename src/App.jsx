f7c7743b4882b02530eb0aac8c955b435ccdc121
Your Name <ervthirumal14321@gmail.com>
2025-10-06 11:12:58 +0530

    f2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './Pages/Home'
-import { Typography } from '@mui/material'
 
 function App() {
   const [count, setCount] = useState(0)
